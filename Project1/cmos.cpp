cbe5e5e8af169b67aa1061c26f7e6cd10fafa42e
Nicolas Dozmati <nd371518@ohio.edu>
2026-03-16 16:37:59 -0400

    main cpp program functional

--- Project1/cmos.cpp
+++ Project1/cmos.cpp
@@ -11,33 +11,45 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct Program {
+    string name;
+    string tokens;
+    vector<int> fingerprints;
+};
 
-string read(const string &file ){
-    string token;
-    string tokenizedstring;
+struct Result {
+    string a;
+    string b;
+    double score;
+};
 
-    ifstream ins(file);
+/* ---------- Remove spaces between tokens ---------- */
 
-    while(ins >> token){
-        tokenizedstring += token;
+string cleanTokens(const string &line) {
+    string cleaned;
+    for(char c : line) {
+        if(c != ' '){
+            cleaned += c;
+        }
     }
-    ins.close();
-    return tokenizedstring;
-    
+    return cleaned;
 }
 
+/* ---------- Create overlapping k-mers ---------- */
 
-vector<string> addkmers(const string &tokens, int k){
+vector<string> addkmers(const string &tokens, int k) {
     vector<string> kmers;
-    for(size_t i = 0; i <= tokens.length(); i++){
-        kmers.emplace_back(tokens.substr(i,k));
+    for(size_t i = 0; i <= tokens.length() - k; i++) {
+        kmers.push_back(tokens.substr(i,k));
     }
     return kmers;
 }
 
+/* ---------- Simple numeric hash ---------- */
 
 int hashkmer(const string &kmer){
     int hashing = 0;
@@ -48,8 +60,7 @@ int hashkmer(const string &kmer){
     return hashing;
 }
 
-
-
+/* ---------- Hash all kmers ---------- */
 
 vector<int> allkmers(const vector<string> &allkmers){
     vector<int> hashes;
@@ -59,6 +70,7 @@ vector<int> allkmers(const vector<string> &allkmers){
     return hashes;
 }
 
+/* ---------- Winnowing window fingerprints ---------- */
 
 vector<int> fingerprints(const vector<int> &hashes, int w){
     vector<int> fingerprints;
@@ -75,24 +87,79 @@ vector<int> fingerprints(const vector<int> &hashes, int w){
     return fingerprints;
 }
 
-int main(){
+/* ---------- Similarity score ---------- */
 
-//test
-
-int w = 4;
-int k = 5;
-
-string tokens = read("tokens.txt");
-vector<string> kmers =  addkmers(tokens, k);
-vector<int> hashes = allkmers(kmers);
-vector<int> print = fingerprints(hashes, w);
-
-for(size_t i = 0; i < print.size(); ++i){
-    cout << print[i] << endl;
+double similarity(const vector<int> &A, const vector<int> &B) {
+    int shared = 0;
+    for(int a : A) {
+        for(int b : B) {
+            if(a == b) {
+                shared++;
+                break;
+            }
+        }
+    }
+    return (double)shared / (A.size() + B.size());
 }
 
+/* ---------- MAIN ---------- */
+
+int main() {
+    const int K = 9;   // k-mer size
+    const int W = 5;   // window size
+
+    vector<Program> programs;
+    ifstream in("tokens.txt");
+
+    string filename;
+    string rest;
+
+    while(in >> filename) {
+        getline(in, rest);
+
+        Program p;
+
+        p.name = filename;
+        p.tokens = cleanTokens(rest);
+
+        vector<string> k = addkmers(p.tokens, K);
+        vector<int> h = allkmers(k);
+
+        p.fingerprints = fingerprints(h, W);
+        programs.push_back(p);
+    }
+
+    /* ---------- Compare all submissions ---------- */
+
+    vector<Result> results;
+
+    for(size_t i = 0; i < programs.size(); i++) {
+        for(size_t j = i+1; j < programs.size(); j++) {
+            double s = similarity(programs[i].fingerprints, programs[j].fingerprints);
+
+            results.push_back({
+                    programs[i].name,
+                    programs[j].name,
+                    s
+            });
+        }
+    }
+
+    /* ---------- Sort by highest similarity ---------- */
+
+    sort(results.begin(), results.end(), [](const Result &a, const Result &b) {
+            return a.score > b.score;
+        });
+
+    /* ---------- Output report ---------- */
+
+    cout << "Similarity Ranking\n\n";
+
+    for(const auto &r : results) {
+        cout << r.score << " "
+             << r.a << " "
+             << r.b << endl;
+    }
+
+    return 0;
 }
-
-
-
-
